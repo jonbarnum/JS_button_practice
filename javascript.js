aed83d34cd5c2a7c3c627e115baedba49f9930d2
Jon Barnumn <jonbarnum07@gmail.com>
2021-09-08 13:13:56 -0600

    working on stop watch

--- javascript.js
+++ javascript.js
@@ -14,3 +14,26 @@ function reset (){
     clicks = 0;
     document.getElementById("readOut").innerHTML = clicks;
 }
+
+// const timer = document.innerHTML("time");
+// const start = document.innerHTML("startTime");
+// const stop = document.innerHTML("stopTime");
+// const reset = document.innerHTML("clearStopWatch");
+
+// let time = 0,
+
+// function showTime() {
+//     time += 1;
+//     timer.innerHTML = toHHMMSS(time);
+// }
+
+// function start() {
+//     interval = setInterval(showtTime, 1000);
+// }
+
+// function stop() {
+//     if (interval) {
+//         clearInterval(interval);
+//         interval = null;
+//     }
+// }
