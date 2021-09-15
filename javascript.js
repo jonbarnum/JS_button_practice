003646e2f25756bc4a115e554c06c9d32211a060
Jon Barnum <jonbarnum07@gmail.com>
2021-09-14 21:04:35 -0600

    javascript and css changes

--- javascript.js
+++ javascript.js
@@ -15,25 +15,59 @@ function reset (){
     document.getElementById("readOut").innerHTML = clicks;
 }
 
-// const timer = document.innerHTML("time");
-// const start = document.innerHTML("startTime");
-// const stop = document.innerHTML("stopTime");
-// const reset = document.innerHTML("clearStopWatch");
 
-// let time = 0,
+// learning from https://dev.to/walternascimentobarroso/creating-a-timer-with-javascript-8b7
 
-// function showTime() {
-//     time += 1;
-//     timer.innerHTML = toHHMMSS(time);
-// }
+let miliSec = 0;
+let sec = 0;
+let min = 0;
+let hr = 0;
 
-// function start() {
-//     interval = setInterval(showtTime, 1000);
-// }
+let stopWatch;
 
-// function stop() {
-//     if (interval) {
-//         clearInterval(interval);
-//         interval = null;
-//     }
-// }
+document.getElementById ('start').onclick = () => start ();
+document.getElementById ("stop").onclick = () => stop ();
+document.getElementById ('clear').onclick = () => clear ();
+
+function start (){
+    stop();
+    stopWatch = setInterval (() => {timer(); }, 10);
+}
+
+function stop (){
+    clearInterval (stopWatch);
+}
+
+function clear (){
+    hr = 0;
+    min = 0;
+    sec = 0;
+    miliSec = 0;
+    document.getElementById ('hour').innerHTML = '00';
+    document.getElementById ('minute').innerHTML = '00';
+    document.getElementById ('seconds').innerHTML = '00';
+    document.getElementById ('milliseconds').innerHTML = '000';
+}
+
+function timer (){
+    if ((miliSec += 10) == 1000) {
+        miliSec = 0;
+        sec++;
+    }
+    if (sec == 60) {
+        sec = 0;
+        min++;
+    }
+    if (min == 60) {
+        min = 0;
+        hr++;
+    }
+    document.getElementById ('hour').innerHTML = returnData(hr);
+    document.getElementById ('minute').innerHTML = returnData (min);
+    document.getElementById ('seconds').innerHTML = returnData (sec);
+    document.getElementById ('milliseconds').innerHTML = returnData (miliSec);
+}
+
+function returnData (input) {
+    return input > 10 ? input : `${input}`
+}
